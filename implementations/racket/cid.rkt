d5f647d98dac51d1dde5a4fd2e2c0b934579a779
Curt Cox <curtcox@gmail.com>
2025-11-16 17:35:56 -0600

    Use CLI sha512 for Racket CID

--- implementations/racket/cid.rkt
+++ implementations/racket/cid.rkt
@@ -1,7 +1,6 @@
 #lang racket
 
-(require net/base64
-         openssl/sha)
+(require net/base64)
 
 (define base-dir
   (simplify-path (build-path (this-expression-source-directory) ".." "..")))
@@ -39,3 +38,25 @@
          examples-dir
          cids-dir
          compute-cid)
+(define (sha512-bytes content)
+  (define in-port (open-input-bytes content))
+  (define out-port (open-output-bytes))
+  (define err-port (open-output-bytes))
+  (define proc (subprocess out-port in-port err-port
+                            "openssl" "dgst" "-sha512" "-binary"))
+  (subprocess-wait proc)
+  (define exit-code (subprocess-status proc))
+  (define stderr-bytes (get-output-bytes err-port))
+  (close-input-port in-port)
+  (close-output-port err-port)
+  (cond
+    [(zero? exit-code)
+     (define digest (get-output-bytes out-port))
+     (close-output-port out-port)
+     digest]
+    [else
+     (close-output-port out-port)
+     (error 'sha512-bytes
+            (string-append "openssl sha512 failed: "
+                           (bytes->string/utf-8 stderr-bytes)))]))
+
