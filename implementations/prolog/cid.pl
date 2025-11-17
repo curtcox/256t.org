e915abcb79f67021f70f94ad2556a174c5872227
Curt Cox <curtcox@gmail.com>
2025-11-16 19:04:58 -0600

    Add Prolog CID implementation

---
+++ implementations/prolog/cid.pl
@@ -0,0 +1,74 @@
+:- module(cid, [compute_cid/2, cids_dir/1]).
+
+:- use_module(library(apply)).
+:- use_module(library(base64)).
+:- use_module(library(crypto)).
+:- use_module(library(filesex)).
+:- use_module(library(lists)).
+:- use_module(library(readutil)).
+
+cids_dir(CidsDir) :-
+    prolog_load_context(directory, Dir),
+    directory_file_path(Dir, '..', ImplDir),
+    directory_file_path(ImplDir, '..', Base0),
+    absolute_file_name(Base0, Base, [file_type(directory)]),
+    directory_file_path(Base, cids, CidsDir).
+
+compute_cid(File, CID) :-
+    read_file_to_codes(File, Bytes, [type(binary)]),
+    length(Bytes, Length),
+    encode_length(Length, Prefix),
+    content_suffix(Length, Bytes, Suffix),
+    atomic_list_concat([Prefix, Suffix], CID).
+
+encode_length(Length, Encoded) :-
+    format(atom(Hex), '~16r', [Length]),
+    atom_codes(Hex, HexCodes),
+    length(HexCodes, HexLen),
+    Pad is 12 - HexLen,
+    pad_codes(Pad, 0'0, PadCodes),
+    append(PadCodes, HexCodes, Padded),
+    hex_codes_bytes(Padded, Bytes),
+    base64url_bytes(Bytes, Encoded).
+
+content_suffix(Length, Bytes, Suffix) :-
+    (   Length =< 64
+    ->  base64url_bytes(Bytes, Suffix)
+    ;   crypto_data_hash(Bytes, Hex, [algorithm(sha512), encoding(octet)]),
+        atom_codes(Hex, HexCodes),
+        hex_codes_bytes(HexCodes, HashBytes),
+        base64url_bytes(HashBytes, Suffix)
+    ).
+
+pad_codes(Pad, _, []) :-
+    Pad =< 0,
+    !.
+pad_codes(Pad, Code, [Code|Rest]) :-
+    Next is Pad - 1,
+    pad_codes(Next, Code, Rest).
+
+hex_codes_bytes(Codes, Bytes) :-
+    phrase(hex_bytes(Bytes), Codes).
+
+hex_bytes([]) -->
+    [].
+hex_bytes([Byte|Rest]) -->
+    hex_value(High),
+    hex_value(Low),
+    {Byte is (High << 4) + Low},
+    hex_bytes(Rest).
+
+hex_value(Value) -->
+    [Code],
+    { code_type(Code, xdigit(Value)) }.
+
+base64url_bytes(Bytes, UrlSafe) :-
+    base64(Bytes, Base64),
+    atom_codes(Base64, Codes),
+    maplist(urlsafe_char, Codes, UrlCodes0),
+    exclude(=(0'='), UrlCodes0, UrlCodes),
+    atom_codes(UrlSafe, UrlCodes).
+
+urlsafe_char(0'+, 0'-) :- !.
+urlsafe_char(0'/, 0'_) :- !.
+urlsafe_char(Code, Code).
